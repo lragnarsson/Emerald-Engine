a55408ea4e57052d0c17649b910c0c9aa6f2c450
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2016-03-25 19:20:54 +0100

    Some additional work on the program structure. Can now load models and render using forward rendering. Should hide all input code somewhere as a next step

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,127 +1,220 @@
 #include "Model.hpp"
 
 
-class Model {
-public:
-    void Draw(GLuint shaderProgram) {
-        for (auto mesh : this->meshes) {
-            mesh.Draw(shaderProgram);
+/* --- MESH --- */
+/* Public Mesh functions */
+void Mesh::draw(GLuint shader_program) {
+    GLuint diffuse_num = 1;
+    GLuint specular_num = 1;
+
+    for(GLuint i = 0; i < this->textures.size(); i++) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        if(this->textures[i]->type == DIFFUSE) {
+            glUniform1i(glGetUniformLocation(shader_program, ("texture_Diffuse" + std::to_string(diffuse_num++)).c_str()), i);
+            glBindTexture(GL_TEXTURE_2D, this->textures[i]->id);
+        }
+        else if(this->textures[i]->type == SPECULAR) {
+            glUniform1i(glGetUniformLocation(shader_program, ("texture_Specular" + std::to_string(specular_num++)).c_str()), i);
+            glBindTexture(GL_TEXTURE_2D, this->textures[i]->id);
         }
     }
 
-    void Load(std::string path) {
-        Assimp::Importer importer;
-        const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
+    glUniform1f(glGetUniformLocation(shader_program, "shininess"), 40.0f);
 
-        if(!scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
-            std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
-            return;
-        }
-        this->directory = path.substr(0, path.find_last_of('/'));
-        this->unfoldAssimpNode(scene->mRootNode, scene);
+    glBindVertexArray(this->VAO);
+    glDrawElements(GL_TRIANGLES, this->index_count, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    for (GLuint i = 0; i < this->textures.size(); i++) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+}
+
+void Mesh::upload_mesh_data(GLuint shader_program) {
+    glGenVertexArrays(1, &this->VAO);
+    glBindVertexArray(this->VAO);
+    glGenBuffers(1, &this->EBO);
+    glGenBuffers(3, this->VBO);
+
+    GLuint* indices = &this->indices[0];
+    GLfloat* vertices = &this->vertices[0];
+    GLfloat* normals = &this->normals[0];
+    GLfloat* tex_coords = &this->tex_coords[0];
+
+    /* Element array buffer */
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_count * sizeof(GLuint), indices, GL_STATIC_DRAW);
+
+    /* Vertex coordinates */
+    glBindBuffer(GL_ARRAY_BUFFER, this->VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(glGetAttribLocation(shader_program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(glGetAttribLocation(shader_program, "in_Position"));
+
+    /* Normal vectors */
+    glBindBuffer(GL_ARRAY_BUFFER, this->VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, 3 * vertex_count * sizeof(GLfloat), normals, GL_STATIC_DRAW);
+    glVertexAttribPointer(glGetAttribLocation(shader_program, "in_Normal"), 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(glGetAttribLocation(shader_program, "in_Normal"));
+
+    /* Texture coordinates */
+    glBindBuffer(GL_ARRAY_BUFFER, this->VBO[2]);
+    glBufferData(GL_ARRAY_BUFFER, 2 * vertex_count * sizeof(GLfloat), tex_coords, GL_STATIC_DRAW);
+    glVertexAttribPointer(glGetAttribLocation(shader_program, "in_TexCoord"), 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(glGetAttribLocation(shader_program, "in_TexCoord"));
+
+    /* Unbind VAO */
+    glBindVertexArray(0);
+}
+
+
+
+/* --- MODEL ---*/
+std::vector<Texture*> Model::loaded_textures;
+
+Model::Model(const std::string path, const GLuint shader_program,
+             const glm::mat4 rot_matrix, const glm::mat4 m2w_matrix) {
+
+        this->rot_matrix = rot_matrix;
+        this->m2w_matrix = m2w_matrix;
+        shader_programs.push_back(shader_program);
+        load(path);
+}
+
+/* Public Model functions */
+void Model::draw(GLuint shader_program) {
+    /* Upload model to world matrix and model rotation for normal calculation */
+    GLuint m2w = glGetUniformLocation(shader_program, "model");
+    glUniformMatrix4fv(m2w, 1, GL_FALSE, glm::value_ptr(this->m2w_matrix));
+    GLuint rot = glGetUniformLocation(shader_program, "modelRot");
+    glUniformMatrix4fv(rot, 1, GL_FALSE, glm::value_ptr(this->rot_matrix));
+
+    for (auto mesh : this->meshes) {
+        mesh.draw(shader_program);
+    }
+}
+
+void Model::load(std::string path) {
+    Assimp::Importer importer;
+    const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenNormals);
+
+    if(!scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+        return;
+    }
+    this->directory = path.substr(0, path.find_last_of('/'));
+    this->unfold_assimp_node(scene->mRootNode, scene);
+}
+
+
+/* Private Model functions */
+void Model::unfold_assimp_node(aiNode* node, const aiScene* scene) {
+    for(GLuint i = 0; i < node->mNumMeshes; i++) {
+        aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+        this->meshes.push_back(this->load_mesh(mesh, scene));
     }
 
+    for(GLuint i = 0; i < node->mNumChildren; i++) {
+        this->unfold_assimp_node(node->mChildren[i], scene);
+    }
+}
 
-private:
-    void unfoldAssimpNode(aiNode* node, const aiScene* scene) {
-        for(GLuint i = 0; i < node->mNumMeshes; i++) {
-            aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-            this->meshes.push_back(this->processMesh(mesh, scene));
-        }
+Mesh Model::load_mesh(aiMesh* mesh, const aiScene* scene) {
+    Mesh m;
 
-        for(GLuint i = 0; i < node->mNumChildren; i++) {
-            this->unfoldAssimpNode(node->mChildren[i], scene);
+    m.index_count = 3 * mesh->mNumFaces;
+    m.vertex_count = mesh->mNumVertices;
+
+    for(GLuint i = 0; i < mesh->mNumVertices; i++) {
+        m.vertices.push_back(mesh->mVertices[i].x);
+        m.vertices.push_back(mesh->mVertices[i].y);
+        m.vertices.push_back(mesh->mVertices[i].z);
+
+        m.normals.push_back(mesh->mNormals[i].x);
+        m.normals.push_back(mesh->mNormals[i].y);
+        m.normals.push_back(mesh->mNormals[i].z);
+
+        if(mesh->mTextureCoords[0]) {
+            m.tex_coords.push_back(mesh->mTextureCoords[0][i].x);
+            m.tex_coords.push_back(mesh->mTextureCoords[0][i].y);
+        } else {
+            m.tex_coords.push_back(0.0f);
+            m.tex_coords.push_back(0.0f);
         }
     }
 
-    Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
-        Mesh m;
-
-        m.indexCount = 3 * mesh->mNumFaces;
-        m.vertexCount = mesh->mNumVertices;
-
-        for(GLuint i = 0; i < mesh->mNumVertices; i++) {
-            m.vertices.push_back(mesh->mVertices[i].x);
-            m.vertices.push_back(mesh->mVertices[i].y);
-            m.vertices.push_back(mesh->mVertices[i].z);
-
-            m.normals.push_back(mesh->mNormals[i].x);
-            m.normals.push_back(mesh->mNormals[i].y);
-            m.normals.push_back(mesh->mNormals[i].z);
-
-            if(mesh->mTextureCoords[0]) {
-                m.texCoords.push_back(mesh->mTextureCoords[0][i].x);
-                m.texCoords.push_back(mesh->mTextureCoords[0][i].y);
-            } else {
-                m.texCoords.push_back(0.0f);
-                m.texCoords.push_back(0.0f);
-            }
+    for(GLuint i = 0; i < mesh->mNumFaces; i++) {
+        aiFace face = mesh->mFaces[i];
+        for(GLuint j = 0; j < face.mNumIndices; j++) {
+            m.indices.push_back(face.mIndices[j]);
         }
-
-        for(GLuint i = 0; i < mesh->mNumFaces; i++) {
-            aiFace face = mesh->mFaces[i];
-            for(GLuint j = 0; j < face.mNumIndices; j++) {
-                m.indices.push_back(face.mIndices[j]);
-            }
-        }
-
-        if(mesh->mMaterialIndex >= 0) {
-            aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
-
-            for(GLuint i = 0; i < material->GetTextureCount(aiTextureType_DIFFUSE); i++) {
-                aiString filepath;
-                material->GetTexture(aiTextureType_DIFFUSE, i, &filepath);
-                Texture* texture;
-                texture = LoadTexture(filepath.C_Str(), this->directory);
-                texture->type = DIFFUSE;
-                texture->path = filepath;
-                m.textures.push_back(texture);
-            }
-
-            for(GLuint i = 0; i < material->GetTextureCount(aiTextureType_SPECULAR); i++) {
-                aiString filepath;
-                material->GetTexture(aiTextureType_SPECULAR, i, &filepath);
-                Texture* texture;
-                texture = LoadTexture(filepath.C_Str(), this->directory);
-                texture->type = SPECULAR;
-                texture->path = filepath;
-                m.textures.push_back(texture);
-            }
-        }
-        m.UploadMeshData();
-        return m;
     }
 
+    if(mesh->mMaterialIndex >= 0) {
+        aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
 
-    Texture* LoadTexture(const char* filename, std::string basepath) {
-        std::string filepath = basepath + "/" + std::string(filename);
-        std::cout << filename << "\n";
-
-        for (uint i = 0; i < loadedModelData.textures.size(); i++) {
-            if (!strcmp(loadedModelData.textures[i]->path.C_Str(), filename)) {
-                return loadedModelData.textures[i];
-            }
+        for(GLuint i = 0; i < material->GetTextureCount(aiTextureType_DIFFUSE); i++) {
+            aiString filepath;
+            material->GetTexture(aiTextureType_DIFFUSE, i, &filepath);
+            Texture* texture;
+            texture = load_texture(filepath.C_Str(), this->directory);
+            texture->type = DIFFUSE;
+            texture->path = filepath;
+            m.textures.push_back(texture);
         }
 
-        int width, height;
-        unsigned char* image = SOIL_load_image(filepath.c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
-
-        /* Upload texture */
-        Texture* texture = new Texture();
-        glGenTextures(1, &texture->id);
-        glBindTexture(GL_TEXTURE_2D, texture->id);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-        SOIL_free_image_data(image);
-
-        loadedModelData.textures.push_back(texture);
-
-        return texture;
+        for(GLuint i = 0; i < material->GetTextureCount(aiTextureType_SPECULAR); i++) {
+            aiString filepath;
+            material->GetTexture(aiTextureType_SPECULAR, i, &filepath);
+            Texture* texture;
+            texture = load_texture(filepath.C_Str(), this->directory);
+            texture->type = SPECULAR;
+            texture->path = filepath;
+            m.textures.push_back(texture);
+        }
     }
-};
+
+    /* Upload for all relevant shader programs */
+    if (!shader_programs.size())
+        std::cerr << "No shader programs attached to model." << std::endl;
+    for (auto program : shader_programs) {
+        m.upload_mesh_data(program);
+    }
+
+    return m;
+}
+
+
+Texture* Model::load_texture(const char* filename, std::string basepath) {
+    std::string filepath = basepath + "/" + std::string(filename);
+    std::cout << filename << "\n";
+
+    for (uint i = 0; i < Model::loaded_textures.size(); i++) {
+        if (!strcmp(Model::loaded_textures[i]->path.C_Str(), filename)) {
+            return Model::loaded_textures[i];
+        }
+    }
+
+    int width, height;
+    unsigned char* image = SOIL_load_image(filepath.c_str(), &width, &height, 0, SOIL_LOAD_RGBA);
+
+    /* Upload texture */
+    Texture* texture = new Texture();
+    glGenTextures(1, &texture->id);
+    glBindTexture(GL_TEXTURE_2D, texture->id);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    SOIL_free_image_data(image);
+
+    Model::loaded_textures.push_back(texture);
+
+    return texture;
+}
+
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,40 +1,136 @@
 #include "Utils.hpp"
 
-/* Loads, compiles and activates vertex and fragment shaders */
-GLuint loadShaders(const GLchar* vertexFilePath, const GLchar* fragmentFilePath)
-{
-    std::string vertexSource;
-    std::string fragmentSource;
-    std::ifstream vShaderFile;
-    std::ifstream fShaderFile;
+Light::Light(const glm::vec3 world_coord, const glm::vec3 ambient_color,
+             const glm::vec3 diffuse_color, const glm::vec3 specular_color) {
 
-    vShaderFile.exceptions(std::ifstream::badbit);
-    fShaderFile.exceptions(std::ifstream::badbit);
+    this->position = world_coord;
+    this->ambient_color = ambient_color;
+    this->diffuse_color = diffuse_color;
+    this->specular_color = specular_color;
+}
+
+void upload_lights(const GLuint shader_program, const std::vector<Light*> &loaded_lights) {
+    for (int i = 0; i < loaded_lights.size(); i++) {
+        glUniform3fv(glGetUniformLocation(shader_program, ("lights[" + std::to_string(i) + "].position").c_str()), 1,
+                     glm::value_ptr(loaded_lights[i]->position));
+        glUniform3fv(glGetUniformLocation(shader_program, ("lights[" + std::to_string(i) + "].ambientColor").c_str()), 1,
+                     glm::value_ptr(loaded_lights[i]->ambient_color));
+        glUniform3fv(glGetUniformLocation(shader_program, ("lights[" + std::to_string(i) + "].diffuseColor").c_str()), 1,
+                     glm::value_ptr(loaded_lights[i]->diffuse_color));
+        glUniform3fv(glGetUniformLocation(shader_program, ("lights[" + std::to_string(i) + "].specularColor").c_str()), 1,
+                     glm::value_ptr(loaded_lights[i]->specular_color));
+    }
+    glUniform1i(glGetUniformLocation(shader_program, "nLights"), loaded_lights.size());
+}
+
+bool sdl_init(const GLuint screen_width, const GLuint screen_height,
+              SDL_Window *&main_window, SDL_GLContext &main_context)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "Failed to init SDL\n";
+        return false;
+    }
+
+    main_window = SDL_CreateWindow("TSBK07 Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                   screen_width, screen_height, SDL_WINDOW_OPENGL);
+    if (!main_window) {
+        std::cerr << "Unable to create window\n";
+        return false;
+    }
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    main_context = SDL_GL_CreateContext(main_window);
+
+#ifndef __APPLE__
+    // Init GLEW
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return false;
+    }
+#endif
+
+    SDL_GL_SetSwapInterval(1);
+    glEnable(GL_DEPTH_TEST);
+    SDL_GL_SwapWindow(main_window);
+
+    return true;
+}
+
+
+void sdl_quit(SDL_Window* main_window, SDL_GLContext &main_context)
+{
+    SDL_GL_DeleteContext(main_context);
+    SDL_DestroyWindow(main_window);
+    SDL_Quit();
+}
+
+
+void sdl_check_error(const int line = -1)
+{
+    std::string error = SDL_GetError();
+
+    if (error != "") {
+        std::cout << "SLD Error : " << error << std::endl;
+
+        if (line != -1) {
+            std::cout << "\nLine : " << line << std::endl;
+        }
+
+        SDL_ClearError();
+    }
+}
+
+
+void sdl_print_attributes()
+{
+    int value = 0;
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MAJOR_VERSION : " << value << std::endl;
+
+    SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &value);
+    std::cout << "SDL_GL_CONTEXT_MINOR_VERSION: " << value << std::endl;
+}
+
+
+/* Loads, compiles and activates vertex and fragment shaders */
+GLuint load_shaders(const GLchar* vertex_file_path, const GLchar* fragment_file_path)
+{
+    std::string vertex_source;
+    std::string fragment_source;
+    std::ifstream vertex_shader_file;
+    std::ifstream fragment_shader_file;
+
+    vertex_shader_file.exceptions(std::ifstream::badbit);
+    fragment_shader_file.exceptions(std::ifstream::badbit);
 
     /* Read shaders from files */
     try {
-        vShaderFile.open(vertexFilePath);
-        fShaderFile.open(fragmentFilePath);
+        vertex_shader_file.open(vertex_file_path);
+        fragment_shader_file.open(fragment_file_path);
 
         std::stringstream vShaderStream, fShaderStream;
-        vShaderStream << vShaderFile.rdbuf();
-        fShaderStream << fShaderFile.rdbuf();
-        vShaderFile.close();
-        fShaderFile.close();
+        vShaderStream << vertex_shader_file.rdbuf();
+        fShaderStream << fragment_shader_file.rdbuf();
+        vertex_shader_file.close();
+        fragment_shader_file.close();
 
-        vertexSource = vShaderStream.str();
-        fragmentSource = fShaderStream.str();
+        vertex_source = vShaderStream.str();
+        fragment_source = fShaderStream.str();
     }
     catch (std::ifstream::failure e) {
         std::cerr << "ERROR: Could not read shader files." << std::endl;
     }
 
-    const GLchar* vSource = vertexSource.c_str();
-    const GLchar* fSource = fragmentSource.c_str();
+    const GLchar* vSource = vertex_source.c_str();
+    const GLchar* fSource = fragment_source.c_str();
 
     GLuint vertex, fragment;
     GLint success;
-    GLchar infoLog[512];
+    GLchar info_log[512];
 
     /* Compile vertex shader */
     vertex = glCreateShader(GL_VERTEX_SHADER);
@@ -42,8 +138,8 @@ GLuint loadShaders(const GLchar* vertexFilePath, const GLchar* fragmentFilePath)
     glCompileShader(vertex);
     glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(vertex, 512, NULL, infoLog);
-        std::cerr << "ERROR: Vertex shader compilation failed: \n" << infoLog << std::endl;
+        glGetShaderInfoLog(vertex, 512, NULL, info_log);
+        std::cerr << "ERROR: Vertex shader compilation failed: \n" << info_log << std::endl;
     }
 
     /* Compile fragment shader */
@@ -52,8 +148,8 @@ GLuint loadShaders(const GLchar* vertexFilePath, const GLchar* fragmentFilePath)
     glCompileShader(fragment);
     glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-        std::cerr << "ERROR: Fragment shader compilation failed: \n" << infoLog << std::endl;
+        glGetShaderInfoLog(fragment, 512, NULL, info_log);
+        std::cerr << "ERROR: Fragment shader compilation failed: \n" << info_log << std::endl;
     }
 
     /* Create shader program */
@@ -63,8 +159,8 @@ GLuint loadShaders(const GLchar* vertexFilePath, const GLchar* fragmentFilePath)
     glLinkProgram(program);
     glGetProgramiv(program, GL_LINK_STATUS, &success);
     if (!success) {
-        glGetProgramInfoLog(program, 512, NULL, infoLog);
-        std::cerr << "ERROR: Shader program linking failed: \n" << infoLog << std::endl;
+        glGetProgramInfoLog(program, 512, NULL, info_log);
+        std::cerr << "ERROR: Shader program linking failed: \n" << info_log << std::endl;
     }
 
     glDeleteShader(vertex);
@@ -74,3 +170,4 @@ GLuint loadShaders(const GLchar* vertexFilePath, const GLchar* fragmentFilePath)
 
     return program;
 }
+
