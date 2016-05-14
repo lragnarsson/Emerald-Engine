63fef6a0f399a04b1cf505c6de78d173088508e5
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2016-05-15 00:37:36 +0200

    Moved scene files to res folder

--- src/main.hpp
+++ src/main.hpp
@@ -17,7 +17,7 @@ Camera camera = Camera(glm::vec3(0.f, 10.f, 3.f),    // Position
                        glm::vec3(0.f, 0.f, -1.f),   // Front
                        glm::vec3(0.0f, 1.0f, 0.0f), // Up
                        glm::vec3(1.f, 0.f, 0.f),    // Right
-                       0.3f,                        // Speed
+                       0.8f,                        // Speed
                        0.01f);                      // Rotational speed
 
 glm::mat4 w2v_matrix;
