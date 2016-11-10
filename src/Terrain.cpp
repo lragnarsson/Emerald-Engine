8dfb31e4aca0579519bb7b1a5b518bb6b5412b2d
hansfilipelo <hansfilipelo@gmail.com>
2016-11-10 10:51:43 +0100

    Think Terrain can load all necessary parts of mesh. Textures left and som polishing of paths

--- src/Terrain.cpp
+++ src/Terrain.cpp
@@ -4,7 +4,14 @@
 using namespace glm;
 
 Terrain::Terrain(std::string heightmap_file)
-{
+{   this->rot_matrix = glm::mat4(1.f);
+    this->scale = 1.f;
+    this->scale_matrix = glm::mat4(1.f);
+    this->world_coord = glm::vec3(0.f);
+    this->move_matrix = glm::translate(glm::mat4(1.f), world_coord);
+    this->m2w_matrix = move_matrix  * rot_matrix * scale_matrix;
+    this->clamp_textures = true;
+    
     load_heightmap(heightmap_file);
 }
 
@@ -50,19 +57,49 @@ void Terrain::load_heightmap(std::string heightmap_file)
 
     for (int z = 0; z < heightmap->h; z++){
         for (int x = 0; x < heightmap->w; x++){
+            float height = get_pixel_height(x, z, heightmap);
             // Create vertices (points in 3D space)
             m->vertices.push_back(x);
-            m->vertices.push_back(get_pixel_height(x, z, heightmap));
+            m->vertices.push_back(height);
             m->vertices.push_back(z);
 
-            // Create a normal for each vertice unless at edges of terrain.
-            // Then normal is pointing straight upward
+            // Create a normal for each vertice
             vec3 normal = get_normal(x, z, heightmap);
             m->normals.push_back(normal.x);
             m->normals.push_back(normal.y);
             m->normals.push_back(normal.z);
+            
+            // Texture coords
+            m->tex_coords.push_back(x);
+            m->tex_coords.push_back(height);
+            m->tex_coords.push_back(z);
+
+            // No bitangents
+            m->tangents.push_back(0.f);
+            m->tangents.push_back(0.f);
+            m->tangents.push_back(0.f);
         }
     }
+
+    // Same default shininess as in Model
+    GLfloat shininess;
+    m->shininess = 86.f / 3.f; // Assimp multiplies shininess by 4 because reasons
+
+    // Use default diffuse and specular maps
+    Texture* diffuse_map;
+    diffuse_map = m->load_texture(DEFAULT_DIFFUSE, DEFAULT_PATH, clamp_textures);
+    diffuse_map->type = DIFFUSE;
+    diffuse_map->path = DEFAULT_PATH;
+    m->diffuse_map = diffuse_map;
+    m->specular_map = m->diffuse_map;
+    
+    // Keep normals as normal map
+    Texture* normal_map;
+    normal_map = m->load_texture(DEFAULT_NORMAL, DEFAULT_PATH, clamp_textures);
+    normal_map->type = NORMAL;
+    normal_map->path = DEFAULT_PATH;
+    m->normal_map = normal_map;
+
 }
 
 
@@ -79,7 +116,7 @@ vec3 Terrain::get_normal(int x, int z, SDL_Surface* image){
         base1 = vec3(x+1, get_pixel_height(x+1, z, image), z) - vec3(x, get_pixel_height(x, z, image), z);
         base2 = vec3(x+1, get_pixel_height(x+1, z+1, image), z+1) - vec3(x, get_pixel_height(x, z, image), z);
         vec3 normal2 = cross(base1, base2);
-        
+
         base1 = vec3(x, get_pixel_height(x, z-1, image), z-1) - vec3(x-1, get_pixel_height(x-1, z-1, image), z-1);
         base2 = vec3(x, get_pixel_height(x, z, image), z) - vec3(x-1, get_pixel_height(x-1, z-1, image), z-1);
         vec3 normal3 = cross(base1, base2);
