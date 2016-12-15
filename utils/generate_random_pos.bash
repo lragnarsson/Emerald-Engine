5bda1fe43f23f0847d97dad9413a48e53076ca47
hansfilipelo <hansfilipelo@gmail.com>
2016-12-15 18:58:55 +0100

    Works on Linux

--- utils/generate_random_pos.bash
+++ utils/generate_random_pos.bash
@@ -12,12 +12,20 @@ height=$2
 scale=$3
 nr_instances=$4
 limit=$5
+rotation=""
+x_pos=""
+z_pos=""
 
 for i in $(seq 1 $nr_instances); do
-
-    rotation=$(jot -r 1 0 360)
-    x_pos=$(jot -r 1 -$limit $limit)
-    z_pos=$(jot -r 1 -$limit $limit)
+    if [ "$(uname -s)" == "Darwin" ]; then
+        rotation=$(jot -r 1 0 360)
+        x_pos=$(jot -r 1 -$limit $limit)
+        z_pos=$(jot -r 1 -$limit $limit)
+    else
+        rotation=$(shuf -i0-360 -n1)
+        x_pos=$(seq -$limit $limit | shuf -n1)
+        z_pos=$(seq -$limit $limit | shuf -n1)
+    fi
     echo "$file_to_load | rel | 0 $rotation 0 | $x_pos $height $z_pos | $scale | -1 -1 | -1"
 done
 
